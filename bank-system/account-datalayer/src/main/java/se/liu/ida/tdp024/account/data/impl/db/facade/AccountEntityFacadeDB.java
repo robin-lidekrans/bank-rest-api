fd1b50796dd3c1fab787600447d1dd3bc9b25ff7
Robin Lidekrans <robli282@student.liu.se>
2021-09-28 16:33:23 +0200

    Add kafka messages to account entity facade

--- bank-system/account-datalayer/src/main/java/se/liu/ida/tdp024/account/data/impl/db/facade/AccountEntityFacadeDB.java
+++ bank-system/account-datalayer/src/main/java/se/liu/ida/tdp024/account/data/impl/db/facade/AccountEntityFacadeDB.java
@@ -5,18 +5,45 @@ import se.liu.ida.tdp024.account.data.api.facade.AccountEntityFacade;
 import se.liu.ida.tdp024.account.data.api.facade.TransactionEntityFacade;
 import se.liu.ida.tdp024.account.data.impl.db.entity.AccountDB;
 import se.liu.ida.tdp024.account.data.impl.db.util.EMF;
+import se.liu.ida.tdp024.account.util.kafka.KafkaTopic;
+import se.liu.ida.tdp024.account.util.kafka.KafkaUtil;
+import se.liu.ida.tdp024.account.util.kafka.KafkaUtilImpl;
 
 import javax.persistence.EntityManager;
 import javax.persistence.LockModeType;
 import javax.persistence.TypedQuery;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AccountEntityFacadeDB implements AccountEntityFacade {
 
     private TransactionEntityFacade transactionEntityFacade;
+    private final KafkaUtil kafkaUtil = new KafkaUtilImpl();
 
+    private final static String CREATE_INIT = "Attempting to write new account to database";
+    private final static String CREATE_SUCCESS = "Writing account to database successful.";
+    private final static String CREATE_ERROR = "Error when writing account to database.";
+
+    private final static String GET_LIST_INIT = "Querying database for accounts.";
+    private final static String GET_LIST_SUCCESS = "Account database query successful, returning results.";
+    private final static String GET_INIT = "Querying account database for account.";
+    private final static String GET_SUCCESS = "Account database query successful, returning result.";
+    private final static String GET_ERROR = "Error when querying account database.";
+
+    private final static String CREDIT_INIT = "Attempting to credit account.";
+    private final static String CREDIT_SUCCESS = "Crediting account successful.";
+    private final static String CREDIT_ERROR = "Error crediting account.";
+    private final static String CREDIT_ERROR_INVALID_ACCOUNT = "Error crediting account: Invalid account";
+
+    private final static String DEBIT_INIT = "Attempting to debit account.";
+    private final static String DEBIT_SUCCESS = "Debiting account successful.";
+    private final static String DEBIT_TRANSACTION_SUCCESS = "Account debit transaction completed.";
+    private final static String DEBIT_ERROR = "Error debititing account.";
+    private final static String DEBIT_ERROR_INVALID_ACCOUNT = "Error debiting account: Invalid account.";
+    private final static String DEBIT_ERROR_INSUFFICIENT_FUNDS = "Error debiting account: Insufficient funds.";
+    
     public AccountEntityFacadeDB(TransactionEntityFacade transactionEntityFacade) {
         this.transactionEntityFacade = transactionEntityFacade;
     }
@@ -24,6 +51,7 @@ public class AccountEntityFacadeDB implements AccountEntityFacade {
     @Override
     public String create(String accountType, String personKey, String bankKey) {
         EntityManager em = EMF.getEntityManager();
+        kafkaUtil.publishMessage(KafkaTopic.TRANSACTION, CREATE_INIT);
         try {
             em.getTransaction().begin();
             Account account = new AccountDB();
@@ -33,9 +61,11 @@ public class AccountEntityFacadeDB implements AccountEntityFacade {
             account.setHoldings(0);
             em.persist(account);
             em.getTransaction().commit();
+            kafkaUtil.publishMessage(KafkaTopic.TRANSACTION, CREATE_SUCCESS);
             return "OK";
         } catch (Exception e) {
             em.getTransaction().rollback();
+            kafkaUtil.publishMessage(KafkaTopic.TRANSACTION, CREATE_ERROR);
             return "FAILED";
         } finally {
             em.close();
@@ -45,11 +75,16 @@ public class AccountEntityFacadeDB implements AccountEntityFacade {
     @Override
     public List<Account> get(String personKey) {
         EntityManager em = EMF.getEntityManager();
+        kafkaUtil.publishMessage(KafkaTopic.TRANSACTION, GET_LIST_INIT);
         try {
             TypedQuery<Account> query = em.createQuery(
                     "SELECT acc FROM AccountDB acc WHERE acc.personKey = :personKey", Account.class);
             query.setParameter("personKey", personKey);
+            kafkaUtil.publishMessage(KafkaTopic.TRANSACTION, GET_LIST_SUCCESS);
             return query.getResultList();
+        } catch (Exception e) {
+            kafkaUtil.publishMessage(KafkaTopic.TRANSACTION, GET_ERROR);
+            return new ArrayList<Account>();
         } finally {
             em.close();
         }
@@ -58,12 +93,15 @@ public class AccountEntityFacadeDB implements AccountEntityFacade {
     @Override
     public Account getAccount(long accountID) {
         EntityManager em = EMF.getEntityManager();
+        kafkaUtil.publishMessage(KafkaTopic.TRANSACTION, GET_INIT);
         try {
             TypedQuery<Account> query = em.createQuery(
                     "SELECT acc FROM AccountDB acc WHERE acc.id = :accountID", Account.class);
             query.setParameter("accountID", accountID);
+            kafkaUtil.publishMessage(KafkaTopic.TRANSACTION, GET_SUCCESS);
             return query.getSingleResult();
         } catch (Exception e) {
+            kafkaUtil.publishMessage(KafkaTopic.TRANSACTION, GET_ERROR);
             return null;
         } finally {
             em.close();
@@ -72,6 +110,7 @@ public class AccountEntityFacadeDB implements AccountEntityFacade {
 
     @Override
     public String debitAccount(Long accountID, int amount) {
+        kafkaUtil.publishMessage(KafkaTopic.TRANSACTION, DEBIT_INIT);
         EntityManager em = EMF.getEntityManager();
         String status;
 
@@ -81,31 +120,37 @@ public class AccountEntityFacadeDB implements AccountEntityFacade {
         // Find returns null if no account was found
         if (account == null) {
             em.getTransaction().commit();
+            kafkaUtil.publishMessage(KafkaTopic.TRANSACTION, DEBIT_ERROR_INVALID_ACCOUNT);
             return "FAILED";
         }
 
         int newBalance = account.getHoldings() - amount;
         if (newBalance < 0)  {
+            kafkaUtil.publishMessage(KafkaTopic.TRANSACTION, DEBIT_ERROR_INSUFFICIENT_FUNDS);
             status = "FAILED";
         } 
         else {
             status = "OK";
             account.setHoldings(newBalance);
+            kafkaUtil.publishMessage(KafkaTopic.TRANSACTION, DEBIT_SUCCESS);
         }
         try {
             em.getTransaction().commit();
         } catch (Exception e) {
             em.getTransaction().rollback();
             em.close();
+            kafkaUtil.publishMessage(KafkaTopic.TRANSACTION, DEBIT_ERROR);
             return "FAILED";
         }
         em.close();
+        kafkaUtil.publishMessage(KafkaTopic.TRANSACTION, DEBIT_TRANSACTION_SUCCESS);
         transactionEntityFacade.create("DEBIT", amount, timeStamp, status, account);
         return status;
     }
 
     @Override
     public String creditAccount(Long accountID, int amount) {
+        kafkaUtil.publishMessage(KafkaTopic.TRANSACTION, CREDIT_INIT);
         EntityManager em = EMF.getEntityManager();
         String status;
 
@@ -115,6 +160,7 @@ public class AccountEntityFacadeDB implements AccountEntityFacade {
         // Find returns null if no account was found
         if (account == null) {
             em.close();
+            kafkaUtil.publishMessage(KafkaTopic.TRANSACTION, CREDIT_ERROR_INVALID_ACCOUNT);
             return "FAILED";
         }
         int newBalance = account.getHoldings() + amount;
@@ -123,11 +169,13 @@ public class AccountEntityFacadeDB implements AccountEntityFacade {
         try {
             em.getTransaction().commit();
         } catch (Exception e) {
+            kafkaUtil.publishMessage(KafkaTopic.TRANSACTION, CREDIT_ERROR);
             em.getTransaction().rollback();
             em.close();
             return "FAILED";
         } 
         em.close();
+        kafkaUtil.publishMessage(KafkaTopic.TRANSACTION, CREDIT_SUCCESS);
         transactionEntityFacade.create("CREDIT", amount, timeStamp, status, account);
         return status;
     }
